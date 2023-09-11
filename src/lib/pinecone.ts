a614506835ca8e2ad98c5a43400d8ec8f1c69b30
Jose Lozano <josecamilo.lozanocet@ucalgary.ca>
2023-09-10 23:06:39 -0600

    FIX

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -12,8 +12,8 @@ export const getPineconeClient = async () => {
     if (!pinecone) {
     pinecone = new PineconeClient();
     await pinecone.init({
-        environment: process.env.PINECONE_ENVIRONMENT!,
-        apiKey: process.env.PINECONE_API_KEY!,
+        environment: process.env.PINECONE_ENVIRONMENT!,      
+	    apiKey: process.env.PINECONE_API_KEY!, 
     });
     }
     return pinecone;
